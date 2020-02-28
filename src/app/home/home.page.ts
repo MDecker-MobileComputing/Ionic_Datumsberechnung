f6dcffd0c165df46d2f4deefae67ea1988c5a69e
MDecker-MobileComputer <mide_42b@yahoo.de>
2020-02-28 20:18:55 +0100

    Zweites Datetime-Element dazu, sichtbar wenn in Abhaengigkeit von Segment-Button.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,16 +11,36 @@ export class HomePage {
 
   _heuteDatum: String = new Date().toISOString();
 
+  _datum2PickerUnsichtbar = true;
+
+
   constructor() {}
 
 
   /**
+   * Event-Handler-Methode für Zustandsänderung des Segment-Buttons.
    *
    * @param event CustomEvent<SegmentChangeEventDetail>
    */
-  async onSegmentButtonGeaendert(event : any) {
+  async onSegmentButtonGeaendert(event: any) {
 
-    console.log(`onSegmentButtonGeaendert wurde ausgelöst: ${event.detail.value}`);
+    let segmentWert = event.detail.value;
+
+    switch (event.detail.value) {
+
+      case "plusMinus":
+          console.log("Modus \"plusMinus\"");
+          this._datum2PickerUnsichtbar = true;
+        break;
+
+      case "differenz":
+          console.log("Modus \"different\"");
+          this._datum2PickerUnsichtbar = false;
+        break;
+
+      default:
+        console.log(`Unerwarteter SegmentWert \"${segmentWert}\".`);
+    }
   }
 
 
